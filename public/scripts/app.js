b5903e1ae6a3dd8eed33dfbfae77c0d298b09e8d
MohanRaj0310 <65529361+MohanRaj0310@users.noreply.github.com>
2021-08-11 14:02:26 +0530

    this fucntionality

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,67 @@
-console.log('hii')
+// console.log("hii");
+
+// function func() {
+//   console.log(this);
+// }
+
+// func();
+
+// const arrowFunc = () => console.log(this);
+
+// console.log(arrowFunc());
+
+// const obj = {
+//   name: "mkei",
+//   func: function () {
+//     return function () {
+//       console.log(this);
+//     };
+//   },
+// };
+
+// let test = obj.func();
+// console.log(test());
+
+// function func2() {
+//   function func() {
+//     console.log(this);
+//   }
+//   func();
+//   return () => {
+//     console.log(this);
+//   };
+// }
+// let test1 = func2();
+// console.log(test1());
+
+// if ("k") {
+//   console.log(this);
+// }
+
+// function func3() {
+//   console.log(this);
+//   // 
+//   return function (){
+//     console.log(this)
+//   }
+// }
+// let funcTest = func3();
+// funcTest()
+
+//Arrow this function
+let arrow = () => {
+  return this
+}
+console.log(arrow())
+
+function func(){
+  let  a = 90
+  return function func (){
+    return function nestFunc (){
+      return a
+    }
+  }
+}
+let test = func()
+let test1 = test()
+console.log(test1())
