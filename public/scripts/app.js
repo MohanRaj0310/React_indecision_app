f45223ae5bd9f1df76e9de589d3f3f18fc5f8c3c
MohanRaj0310 <65529361+MohanRaj0310@users.noreply.github.com>
2021-08-13 12:01:58 +0530

    initial commit

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,111 +1,11 @@
-// console.log("hii");
+'use strict';
 
-// function func() {
-//   console.log(this);
-// }
-
-// func();
-
-// const arrowFunc = () => console.log(this);
-
-// console.log(arrowFunc());
-
-// const obj = {
-//   name: "mkei",
-//   func: function () {
-//     return function () {
-//       console.log(this);
-//     };
-//   },
-// };
-
-// let test = obj.func();
-// console.log(test());
-
-// function func2() {
-//   function func() {
-//     console.log(this);
-//   }
-//   func();
-//   return () => {
-//     console.log(this);
-//   };
-// }
-// let test1 = func2();
-// console.log(test1());
-
-// if ("k") {
-//   console.log(this);
-// }
-
-// function func3() {
-//   console.log(this);
-//   // 
-//   return function (){
-//     console.log(this)
-//   }
-// }
-// let funcTest = func3();
-// funcTest()
-
-//Arrow this function
-// let arrow = () => {
-//   return this
-// }
-// console.log(arrow())
-
-// function func(){
-//   let  a = 90
-//   return function func (){
-//     return function nestFunc (){
-//       return a
-//     }
-//   }
-// }
-// let test = func()
-// let test1 = test()
-// console.log(test1())
-
-let template = React.createElement(
-  'p',
-  {
-    id: "connector",
-  },
-  "successfully created p tag"
-);
-  
-let appRoot = document.getElementById('app')
-
-ReactDOM.render(template, appRoot)
-console.log(template)
-console.log(appRoot)
-
-// try another one
-let templateh1 = React.createElement(
+var template = React.createElement(
   'h1',
-  {
-    id: 'header'
-  },
-  'i was created by react .i\'m from app.js'
-)
-ReactDOM.render(templateh1,appRoot)///***********************created a new element over in client side javascript when the same div already has an element ? it is going to be overriding the existing one.**/
+  null,
+  'i\'m from scripts/app.js'
+);
 
-/*
-let templateh1 = React.createElement(
-  "div",
-  {
-    id: "header"
-  }
-)
-ReactDOM.render(templateh1)// react-dom.js:17859 Uncaught Invariant Violation: _registerComponent(...): Target container is not a DOM element.
-*/
+var appRoot = document.getElementById('app');
 
-//testing typeof react subsidary
-// console.log(typeof React.createElement())//object//react.js:3528 Warning: React.createElement: type is invalid -- expected a string (for built-in comp..
-// console.log(typeof ReactDOM.render())//Uncaught Invariant Violation: ReactDOM.render(): Invalid component element.....
-// console.log(typeof React.createElement())
-// console.log(ReactDOM.render())////Uncaught Invariant Violation: ReactDOM.render(): Invalid component element.....
-// console.log(ReactDOM.render())//Uncaught Invariant Violation
-console.log(React.createElement)
-console.log(ReactDOM.render)
-
+ReactDOM.render(template, appRoot);
---
+++ src/app.js
@@ -0,0 +1,5 @@
+let template = <h1>i'm from scripts/app.js</h1>
+
+let appRoot = document.getElementById('app')
+
+ReactDOM.render(template, appRoot)
