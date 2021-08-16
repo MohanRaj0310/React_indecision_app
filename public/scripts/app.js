85f6b3fffe7d5f18eb759663655a96565e85509a
MohanRaj0310 <65529361+MohanRaj0310@users.noreply.github.com>
2021-08-16 16:46:43 +0530

    initial commit

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,34 +1,205 @@
 'use strict';
 
+// let template = <h1>i'm from scripts/app.js</h1>
+
+// let appRoot = document.getElementById('app')
+
+// ReactDOM.render(template, appRoot)
+// // console.log()
+// document.write('jufruhf')
+// console.log('dedede')
+// console.log('dihei')
+
+// let i = 90;
+
+// const func = (x,y,r) => {
+//   console.log('vji')
+// }
+
+
+// let webPageContent = <p>frkgitigtr</p>
+// let domHTML = document.getElementById('id1')
+// ReactDOM.render(webPageContent, domHTML)
+
+// let template = <h1> Indecision app</h1><p
+// >djirf/p</p>;
+
+//dynamic template
+
+var user = {
+  name: 'mohan',
+  age: 23,
+  text: 'from object',
+  str: '',
+  boolVal: false,
+  birthYear: function birthYear() {
+    return this.age;
+  },
+  template: React.createElement(
+    'h1',
+    null,
+    'temoal'
+  ),
+  func: function func() {
+    return React.createElement(
+      'h1',
+      null,
+      this.text
+    );
+  },
+  options: [1, 2, 3],
+  title: 'app Name',
+  subTitle: 'crocodile story'
+
+  // user.ageCalc();
+  // let x;
+};console.log(template); //undefined
+console.log(c);
+var c = 90;
+var y = null;
+var defined = undefined;
+var str = '';
+var val = 0;
 var template = React.createElement(
-  'h1',
+  'div',
   null,
-  'i\'m from scripts/app.js'
+  React.createElement(
+    'h1',
+    null,
+    user.name
+  ),
+  React.createElement(
+    'p',
+    null,
+    user.birthYear()
+  ),
+  React.createElement(
+    'p',
+    null,
+    false,
+    ' ',
+    undefined
+  ),
+  React.createElement(
+    'p',
+    null,
+    user.birthYear()
+  ),
+  React.createElement(
+    'p',
+    null,
+    null
+  ),
+  React.createElement(
+    'p',
+    null,
+    user.birthYear()
+  ),
+  React.createElement(
+    'p',
+    null,
+    user.birthYear()
+  ),
+  React.createElement(
+    'p',
+    null,
+    user.boolVal
+  ),
+  React.createElement(
+    'p',
+    null,
+    user.str
+  ),
+  React.createElement(
+    'p',
+    null,
+    user.birthYear()
+  ),
+  React.createElement(
+    'h2',
+    null,
+    undefined
+  ),
+  React.createElement(
+    'h3',
+    null,
+    console.log('im from jsx')
+  ),
+  React.createElement(
+    'p',
+    null,
+    undefined
+  ),
+  1 ? React.createElement(
+    'p',
+    null,
+    'paragrapgh'
+  ) : React.createElement(
+    'h1',
+    null,
+    'dkooe'
+  ),
+  user.func(),
+  user.template,
+  React.createElement(
+    'p',
+    null,
+    user.birthYear()
+  ),
+  React.createElement(
+    'p',
+    null,
+    0
+  ),
+  y,
+  defined,
+  val,
+  str,
+  func(),
+  user.age >= 18 && React.createElement(
+    'p',
+    null,
+    user.age + 100
+  ),
+  user.subTitle && React.createElement(
+    'p',
+    null,
+    user.subTitle
+  ),
+  user.options.length > 0 && React.createElement(
+    'p',
+    null,
+    'here ARE OPTIONS : ',
+    user.options
+  ),
+  user.unknown || React.createElement(
+    'p',
+    null,
+    'user.known is not exist'
+  )
 );
 
+var templateTwo = template;
 var appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
-// console.log()
-document.write('jufruhf');
-console.log('dedede');
-console.log('dihei');
+console.log(appRoot);
+// console.log(x)
 
-var i = 90;
+function func(x) {
+  if (x) {
+    return 0;
+    console.log('deyded');
+  }
+}
 
-var func = function func(x, y, r) {
-  console.log('vji');
-};
+console.log(func(null)); //undefined
+console.log(null);
 
-var webPageContent = React.createElement(
-  'p',
-  null,
-  'frkgitigtr'
-);
-var domHTML = document.getElementById('id1');
-let x = ReactDOM.render(webPageContent, domHTML);
-console.log('ftgr')
-
-let set = 0
-console.log(set)
-console.log(x)//<p data-reactroot="">frkgitigtr</p>
+console.log(0 && 'heudueude');
+console.log(1 && true);
+console.log(1 || true);
+console.log(user.age >= 25 && true);
+console.log([1, 2, 3]);
+var array = ['swdhu', 24333, 73434783478];
+console.log(array);
--- src/app.js
+++ src/app.js
@@ -1,20 +1,110 @@
-let template = <h1>i'm from scripts/app.js</h1>
+// let template = <h1>i'm from scripts/app.js</h1>
 
+// let appRoot = document.getElementById('app')
+
+// ReactDOM.render(template, appRoot)
+// // console.log()
+// document.write('jufruhf')
+// console.log('dedede')
+// console.log('dihei')
+
+// let i = 90;
+
+// const func = (x,y,r) => {
+//   console.log('vji')
+// }
+
+
+// let webPageContent = <p>frkgitigtr</p>
+// let domHTML = document.getElementById('id1')
+// ReactDOM.render(webPageContent, domHTML)
+
+// let template = <h1> Indecision app</h1><p
+// >djirf/p</p>;
+
+//dynamic template
+
+let user = {
+  name: 'mohan',
+  age: 23,
+  text: 'from object',
+  str: '',
+  boolVal : false,
+  birthYear : function (){
+    return this.age;
+  },
+  template : <h1>temoal</h1>,
+  func: function (){
+    return <h1>{this.text}</h1>
+  },
+  options: [1, 2, 3],
+  title: 'app Name',
+  subTitle: 'crocodile story'
+}
+  
+
+
+// user.ageCalc();
+// let x;
+console.log(template)//undefined
+console.log(c)
+let c = 90;
+let y = null
+let defined = undefined
+let str = ''
+let val = 0
+var template = (<div>
+  <h1>{user.name}</h1>
+  <p>{user.birthYear()}</p>
+  <p>{false} {undefined}</p>
+  
+  <p>{user.birthYear()}</p>
+  <p>{null}</p>
+  <p>{user.birthYear()}</p>
+  <p>{user.birthYear()}</p>
+  <p>{user.boolVal}</p>
+  <p>{user.str}</p>
+  <p>{user.birthYear()}</p>
+  <h2>{this}</h2>
+  <h3>{console.log('im from jsx')}</h3>
+  <p>{undefined}</p>
+  {1?<p>paragrapgh</p>:<h1>dkooe</h1>}
+  {user.func()}
+  {user.template}
+  <p>{user.birthYear()}</p>
+  <p>{0}</p>
+  {y}
+  {defined}
+  {val}
+  {str}
+  {func()}
+  {user.age >= 18 && <p>{user.age + 100}</p>}
+  {user.subTitle && <p>{user.subTitle}</p>}
+  {user.options.length > 0 && <p>here ARE OPTIONS : {user.options}</p>}
+  {user.unknown || <p>user.known is not exist</p>}
+</div>);
+
+let templateTwo = template;
 let appRoot = document.getElementById('app')
 
 ReactDOM.render(template, appRoot)
-// console.log()
-document.write('jufruhf')
-console.log('dedede')
-console.log('dihei')
+console.log(appRoot)
+// console.log(x)
 
-let i = 90;
-
-const func = (x,y,r) => {
-  console.log('vji')
+function func (x){
+  if(x){
+    return 0
+    console.log('deyded')
+  }
 }
 
+console.log(func(null))//undefined
+console.log(null)
 
-let webPageContent = <p>frkgitigtr</p>
-let domHTML = document.getElementById('id1')
-ReactDOM.render(webPageContent, domHTML)
+console.log(0 && 'heudueude')
+console.log(1 && true)
+console.log(1 || true)
+console.log(user.age >= 25 && true)
+console.log([1, 2, 3])
+let array = ['swdhu',24333,73434783478]
+console.log(array)
