ee10852573f33b64199b1eaef342238a9f1981c4
MohanRaj0310 <65529361+MohanRaj0310@users.noreply.github.com>
2021-08-16 09:34:50 +0530

    initial commit

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -9,3 +9,26 @@ var template = React.createElement(
 var appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
+// console.log()
+document.write('jufruhf');
+console.log('dedede');
+console.log('dihei');
+
+var i = 90;
+
+var func = function func(x, y, r) {
+  console.log('vji');
+};
+
+var webPageContent = React.createElement(
+  'p',
+  null,
+  'frkgitigtr'
+);
+var domHTML = document.getElementById('id1');
+let x = ReactDOM.render(webPageContent, domHTML);
+console.log('ftgr')
+
+let set = 0
+console.log(set)
+console.log(x)//<p data-reactroot="">frkgitigtr</p>
--- src/app.js
+++ src/app.js
@@ -3,3 +3,18 @@ let template = <h1>i'm from scripts/app.js</h1>
 let appRoot = document.getElementById('app')
 
 ReactDOM.render(template, appRoot)
+// console.log()
+document.write('jufruhf')
+console.log('dedede')
+console.log('dihei')
+
+let i = 90;
+
+const func = (x,y,r) => {
+  console.log('vji')
+}
+
+
+let webPageContent = <p>frkgitigtr</p>
+let domHTML = document.getElementById('id1')
+ReactDOM.render(webPageContent, domHTML)
